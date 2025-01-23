22260a2136b4ba02351305d0c0a09536a7e15402
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2025-01-23 20:01:41 -0300

    Adiciona a entidade Endereco e remove exclusão do lombok no pom.xml

---
+++ collect-backend/src/main/java/com/ifba/topicosbd/collect/core/entities/Endereco.java
@@ -0,0 +1,30 @@
+package com.ifba.topicosbd.collect.core.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "endereco")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @Column(name = "cep", nullable = false)
+    private String cep;
+    @Column(name = "cidade", nullable = false)
+    private String cidade;
+    @Column(name = "rua", nullable = false)
+    private String Rua;
+    @Column(name = "numero")
+    private Integer numero;
+    @Column(name = "complemento")
+    private String complemento;
+
+}
